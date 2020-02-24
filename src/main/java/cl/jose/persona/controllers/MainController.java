c18712faadb3675c812fbcc546089873177414f6
jnieto <jnieto@cellit.cl>
2020-02-23 22:19:22 -0300

    Commit de test

--- src/main/java/cl/jose/persona/controllers/MainController.java
+++ src/main/java/cl/jose/persona/controllers/MainController.java
@@ -12,7 +12,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.json.JSONObject;T
+import org.json.JSONObject;
 
 import java.util.concurrent.atomic.AtomicLong;
 
