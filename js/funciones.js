6ee5c55024522c4577ce0b7136387ee8cecca1d1
joseenietoo <josee.nietoo@gmail.com>
2020-02-24 10:42:53 -0300

    Update tamaño ventana

--- js/funciones.js
+++ js/funciones.js
@@ -3,6 +3,6 @@ function consultar() {
     var nombreCompleto = f.name.value
     var fechaNacimiento = f.birthday.value
     var url = "http://localhost:8080/persona?nombreCompleto="+nombreCompleto+"&fechaNacimiento="+fechaNacimiento;
-    myWindow = window.open(url, '', 'width=800,height=200,scrollbars=1');
+    myWindow = window.open(url, '', 'width=1000,height=1000,scrollbars=1');
     myWindow.focus();
 }
